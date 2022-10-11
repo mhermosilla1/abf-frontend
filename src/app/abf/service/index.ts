86a46139c7353696d4d591a36dc301c81ca66947
Marta H <martitah-1@fpuna.edu.py>
2022-10-10 21:36:34 -0300

    Edicion de componentes con errores (academico),
    compilación ok con adición de nuevos elementos en el home

--- src/app/abf/service/index.ts
+++ src/app/abf/service/index.ts
@@ -1,3 +1,4 @@
 export { TiposDocumentosService } from "./tipos-documentos.service";
 export { RolesService } from "./roles.service";
 export { DummyService } from "./dummy.service";
+export { TemasService } from "./temas.service";
